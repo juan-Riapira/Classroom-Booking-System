6a93087a24759db95ed599d4e8248c670c0966ad
Juan-Fernandez <juan.fernandez07@uptc.edu.co>
2025-11-05 15:13:28 -0500

    Initial commit: project structure

---
+++ reporting service/reporting-service/src/test/java/co/edu/uptc/reporting/service/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.reporting.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
